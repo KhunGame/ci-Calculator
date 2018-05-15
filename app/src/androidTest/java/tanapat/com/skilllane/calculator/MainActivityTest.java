32686a2ca948cc4b6f423d342d1ef4966ac08629
KhunGame <Tanapat.kit@gmail.com>
2018-05-15 12:12:24 +0700

    remove screen shot

--- app/src/androidTest/java/tanapat/com/skilllane/calculator/MainActivityTest.java
+++ app/src/androidTest/java/tanapat/com/skilllane/calculator/MainActivityTest.java
@@ -6,8 +6,6 @@ import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 
-import tools.fastlane.screengrab.Screengrab;
-import tools.fastlane.screengrab.UiAutomatorScreenshotStrategy;
 import tools.fastlane.screengrab.locale.LocaleTestRule;
 
 import static android.support.test.espresso.Espresso.onView;
@@ -31,17 +29,17 @@ public class MainActivityTest {
     @Test
     public void plus_1_and_1_should_result_2() {
         //set for screen grab
-        Screengrab.setDefaultScreenshotStrategy(new UiAutomatorScreenshotStrategy());
+//        Screengrab.setDefaultScreenshotStrategy(new UiAutomatorScreenshotStrategy());
         CalculatorPage.กรอกเลขตัวแรก("1");
 
-        Screengrab.screenshot("step01");
+//        Screengrab.screenshot("step01");
 
         onView(withId(R.id.btn_plus))
                 .perform(click());
 
         CalculatorPage.กรอกเลขตัวที่สอง("1");
 
-        Screengrab.screenshot("step02");
+//        Screengrab.screenshot("step02");
 
         onView(withId(R.id.btn_calculator))
                 .perform(click());
@@ -49,6 +47,6 @@ public class MainActivityTest {
         onView(withId(R.id.txv_result))
                 .check(matches(withText("Result = 2")));
 
-        Screengrab.screenshot("step03");
+//        Screengrab.screenshot("step03");
     }
 }
